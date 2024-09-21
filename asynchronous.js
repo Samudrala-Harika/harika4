34874a78feeb436d5f9257d30a1812e0db1b0887
Samudrala-Harika <harikasamudrala03@gmail.com>
2024-09-21 18:23:54 +0530

    synchronous and asynchronous functions

---
+++ asynchronous.js
@@ -0,0 +1,6 @@
+console.log("Hiiiiiiiiiiiii");
+setTimeout(()=>{
+    console.log("This is asynhronous statement")
+},3000)
+console.log("Hellooooooooooooooo");
+console.log("Welcome to JavaScript");
